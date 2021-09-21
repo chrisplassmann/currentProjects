09ff360462339d910d671bde6588ef8e498a427f
chrisplassmann <chris.plassmann@nerdpower.energy>
2021-09-21 15:40:53 -0500

    new commit

--- beginner-javascript-master/exercises/79 - Currency Converter Modules Refactor  - FINISHED/lib.js
+++ beginner-javascript-master/exercises/79 - Currency Converter Modules Refactor  - FINISHED/lib.js
@@ -1,26 +1,26 @@
 const endpoint = 'http://api.currencylayer.com/live';
-const accessKey = process.env.CURRENCY_API_KEY;
-const ratesByBase = {};
+// const accessKey = process.env.CURRENCY_API_KEY;
+const accessKey = '07f474a70321150767f24b6e14e28019';
 
-export async function fetchRates(base = 'USD') {
-  const res = await fetch(`${endpoint}?base=${base}`);
-  const rates = await res.json();
-  return rates;
+export async function fetchRates() {
+  const res = fetch(`${endpoint}?access_key=${accessKey}`);
+  const origin = await (await res).json();
+  const { quotes } = origin;
+  return quotes;
 }
 
 export async function convert(amount, from, to) {
-  // first check if we even have the rates to convert from that currency
-  if (!ratesByBase[from]) {
-    console.log(
-      `Oh no, we dont have ${from} to convert to ${to}. So gets go get it!`
-    );
-    const rates = await fetchRates(from);
-    console.log(rates);
-    // store them for next time
-    ratesByBase[from] = rates;
-  }
-  // convert that amount that they passed it
-  const rate = ratesByBase[from].rates[to];
+  // first check if we have the rates to convert
+  console.log(`We dont have ${from} to convert to ${to}. Getting it now.`);
+  const quotes = await fetchRates();
+  console.log(quotes);
+  const fromTo = [from, to].join('');
+  console.log(fromTo);
+  const rate = quotes[fromTo];
+  console.log(rate);
+  console.log(Object.values(quotes));
+  // store for next time
+
   const convertedAmount = rate * amount;
   console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
   return convertedAmount;
